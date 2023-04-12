1a1f44d7033cbf2a3bf93795dc66245e641925df
Tiago Carvalho <tsc2@cin.ufpe.br>
2023-04-12 03:13:14 -0300

    gg

--- frontend/application/frontend/views.py
+++ frontend/application/frontend/views.py
@@ -1,6 +1,5 @@
 # application/frontend/views.py
 import requests
-from . import forms
 from . import frontend_blueprint
 from .api.Fachada import Fachada
 from flask import render_template, session, redirect, url_for, flash, request
@@ -17,7 +16,7 @@ def home():
             'results': []
         }
 
-    return render_template('home/index.html', gastos=gastos['results'], orcamentos=orcamentos['results'])
+    return render_template('gasto/index.html', gastos=gastos['results'], orcamentos=orcamentos['results'])
 
 
 @frontend_blueprint.route('/criar-gasto', methods=['GET', 'POST'])
@@ -37,9 +36,9 @@ def criar_gasto():
 
     elif request.method == 'GET':
         orcamentos = Fachada.get_orcamentos()
-        return render_template('home/gasto_form.html', orcamentos=orcamentos['results'])
+        return render_template('gasto/post_form.html', orcamentos=orcamentos['results'])
 
-    return render_template("home/gasto_form.html")
+    return render_template("gasto/post_form.html")
 
 
 @frontend_blueprint.route('/sincronizar-gastos', methods=['GET', 'POST'])
@@ -56,9 +55,9 @@ def sincronizar_gastos():
         Fachada.sync_gastos(payload)
 
     elif request.method == 'GET':
-        return render_template('home/sync_form.html')
+        return render_template('gasto/sync_form.html')
 
-    return render_template("home/sync_form.html")
+    return render_template("gasto/sync_form.html")
 
 
 @frontend_blueprint.route('/cadastrar-orcamento', methods=['GET', 'POST'])
@@ -89,97 +88,3 @@ def home_orcamento():
         }
 
     return render_template('orcamento/index.html', orcamentos=orcamentos['results'])
-
-
-'''
-@frontend_blueprint.route('/register', methods=['GET', 'POST'])
-def register():
-    form = forms.RegistrationForm(request.form)
-    if request.method == "POST":
-        if form.validate_on_submit():
-            username = form.username.data
-
-            # Search for existing user
-            user = UserClient.does_exist(username)
-            if user:
-                # Existing user found
-                flash('Please try another username', 'error')
-                return render_template('register/index.html', form=form)
-            else:
-                # Attempt to create new user
-                user = UserClient.post_user_create(form)
-                if user:
-                    flash('Thanks for registering, please login', 'success')
-                    return redirect(url_for('frontend.login'))
-
-        else:
-            flash('Errors found', 'error')
-
-    return render_template('register/index.html', form=form)
-
-
-@frontend_blueprint.route('/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('frontend.home'))
-    form = forms.LoginForm()
-    if request.method == "POST":
-        if form.validate_on_submit():
-            api_key = UserClient.post_login(form)
-            if api_key:
-                session['user_api_key'] = api_key
-                user = UserClient.get_user()
-                session['user'] = user['result']
-
-                order = OrderClient.get_order()
-                if order.get('result', False):
-                    session['order'] = order['result']
-
-                flash('Welcome back, ' + user['result']['username'], 'success')
-                return redirect(url_for('frontend.home'))
-            else:
-                flash('Cannot login', 'error')
-        else:
-            flash('Errors found', 'error')
-    return render_template('login/index.html', form=form)
-
-@frontend_blueprint.route('/logout', methods=['GET'])
-def logout():
-    session.clear()
-    return redirect(url_for('frontend.home'))
-
-
-@frontend_blueprint.route('/checkout', methods=['GET'])
-def summary():
-    if 'user' not in session:
-        flash('Please login', 'error')
-        return redirect(url_for('frontend.login'))
-
-    if 'order' not in session:
-        flash('No order found', 'error')
-        return redirect(url_for('frontend.home'))
-    order = OrderClient.get_order()
-
-    if len(order['result']['items']) == 0:
-        flash('No order found', 'error')
-        return redirect(url_for('frontend.home'))
-
-    OrderClient.post_checkout()
-
-    return redirect(url_for('frontend.thank_you'))
-
-@frontend_blueprint.route('/order/thank-you', methods=['GET'])
-def thank_you():
-    if 'user' not in session:
-        flash('Please login', 'error')
-        return redirect(url_for('frontend.login'))
-
-    if 'order' not in session:
-        flash('No order found', 'error')
-        return redirect(url_for('frontend.home'))
-
-    session.pop('order', None)
-    flash('Thank you for your order', 'success')
-
-    return render_template('order/thankyou.html')
-'''
