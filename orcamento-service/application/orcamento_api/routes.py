175b5f04add80f7e0ecac4b1545f69284c0f8ce9
Igor Simões <igor.simoes@incognia.com>
2023-04-12 00:57:40 -0300

    fix orcamento

--- orcamento-service/application/orcamento_api/routes.py
+++ orcamento-service/application/orcamento_api/routes.py
@@ -1,11 +1,11 @@
-# application/user_api/routes.py
+# application/orcamento_api/routes.py
 from . import orcamento_api_blueprint
-from .. import db, login_manager
+from .. import db
 from ..models import Orcamento
 from flask import make_response, request, jsonify
 
 
-@orcamento_api_blueprint.route('/api/orcamento', methods=['GET'])
+@orcamento_api_blueprint.route('/api/orcamentos', methods=['GET'])
 def get_orcamentos():
     data = []
     for row in Orcamento.query.all():
@@ -17,28 +17,13 @@ def get_orcamentos():
 
 @orcamento_api_blueprint.route('/api/orcamento/create', methods=['POST'])
 def create_orcamento():
-    nome = request.form['nome']
-    mes = request.form['mes']
-    valor_maximo = request.form['valor_maximo']
-
-    orcamento = orcamento()
-    orcamento.nome = nome
-    orcamento.mes = mes
-    orcamento.valor_maximo = valor_maximo
+    orcamento = Orcamento()
+    orcamento.nome = request.form['nome']
+    orcamento.mes = request.form['mes']
+    orcamento.valor_maximo = request.form['valor_maximo']
 
     db.session.add(orcamento)
     db.session.commit()
 
-    response = jsonify({'message': 'Orcamento added',
-                       'result': orcamento.to_json()})
-
+    response = jsonify({'message': 'Orcamento added', 'result': orcamento.to_json()})
     return response
-
-# @orcamento_api_blueprint.route('/api/orcamento/<username>/exists', methods=['GET'])
-# def get_orcame(username):
-#     item = User.query.filter_by(username=username).first()
-#     if item is not None:
-#         response = jsonify({'result': True})
-#     else:
-#         response = jsonify({'message': 'Cannot find username'}), 404
-#     return response
